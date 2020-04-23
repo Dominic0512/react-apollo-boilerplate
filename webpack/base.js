b24ff447a21607b77446e3981c95c247b99399b3
Dominic Lee <dominiclee0512@gmail.com>
2020-04-23 23:28:06 +0800

    chore: separate webpack config files.

---
+++ webpack/base.js
@@ -0,0 +1,38 @@
+const appRoot = require("app-root-path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: appRoot.resolve("src/index.js"),
+  output: {
+    filename: "[name].[contenthash].js",
+    path: appRoot.resolve("dist"),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebPackPlugin({
+      filename: "index.html",
+      inject: true,
+      template: appRoot.resolve("src/index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
