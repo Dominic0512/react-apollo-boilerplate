75a8e65517d4ff4a37027f763bc8c9b559b67be0
Dominic Lee <dominiclee0512@gmail.com>
2020-04-26 11:05:26 +0800

    chore: refactor apollo-boost with apollo-client and add basic react boostrap login page.

--- src/config/apollo.js
+++ src/config/apollo.js
@@ -1,7 +1,84 @@
-import ApolloClient from "apollo-boost";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
+import { ApolloLink } from "apollo-link";
+import { withClientState } from "apollo-link-state";
+import { setContext } from "apollo-link-context";
+
+const cache = new InMemoryCache();
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      "x-token": token ? `${token}` : "",
+    },
+  };
+});
+
+const requestLink = new ApolloLink(
+  (operation, forward) =>
+    new Observable((observer) => {
+      let handle;
+      Promise.resolve(operation)
+        .then((operation) => request(operation))
+        .then(() => {
+          handle = forward(operation).subscribe({
+            next: observer.next.bind(observer),
+            error: observer.error.bind(observer),
+            complete: observer.complete.bind(observer),
+          });
+        })
+        .catch(observer.error.bind(observer));
+
+      return () => {
+        if (handle) handle.unsubscribe();
+      };
+    })
+);
+
+const httpLink = new HttpLink({
+  uri: process.env.GRAPHQL_URI,
+  credentials: "same-origin",
+});
+
+const stateLink = withClientState({
+  defaults: {
+    isConnected: true,
+  },
+  resolvers: {
+    Mutation: {
+      updateNetworkStatus: (_, { isConnected }, { cache }) => {
+        cache.writeData({ data: { isConnected } });
+        return null;
+      },
+    },
+  },
+  cache,
+});
 
 const client = new ApolloClient({
-  uri: process.env.GRAPHQL_URI,
+  link: ApolloLink.from([
+    errorLink,
+    authLink,
+    requestLink,
+    stateLink,
+    httpLink,
+  ]),
+  cache,
 });
 
 export default client;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from "react-dom";
 import client from "./config/apollo";
 import { ApolloProvider } from "@apollo/react-hooks";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Login from "./page/Login";
+
 const App = () => (
   <ApolloProvider client={client}>
     <div>
       <h2>My first Apollo app 🚀</h2>
+      <Login></Login>
     </div>
   </ApolloProvider>
 );
