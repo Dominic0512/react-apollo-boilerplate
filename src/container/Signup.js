d4119a0df2101e72e29818c8b6a2cf2fb1908a34
Dominic Lee <dominiclee0512@gmail.com>
2020-07-13 11:36:15 +0800

    feat: implement form validation with react hook form and yup for signup page.

--- src/container/Signup.js
+++ src/container/Signup.js
@@ -1,73 +1,101 @@
-import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useHistory } from 'react-router-dom'
+import { useMutation } from '@apollo/react-hooks'
+import { useForm } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers'
+import * as yup from 'yup'
 
-import { useMutation } from "@apollo/react-hooks";
-import idx from "idx";
+import idx from 'idx'
 
-import { SIGNUP } from "./schema.gql";
+import { SIGNUP } from './schema.gql'
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(8).required(),
+  confirmPassword: yup
+    .string()
+    .min(8)
+    .oneOf([yup.ref('password'), null], 'Passwords must match.')
+    .required(),
+})
 
 const Signup = () => {
-  const history = useHistory();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
+  const history = useHistory()
+
+  const { errors, register, handleSubmit } = useForm({
+    mode: 'onBlur',
+    reValidateMode: 'onBlur',
+    defaultValues: {},
+    resolver: yupResolver(schema),
+  })
 
   const [signup, signupResp] = useMutation(SIGNUP, {
     onCompleted: (resp) => {
-      const user = idx(resp, (_) => _.signup.user);
-      console.log(user);
+      const user = idx(resp, (_) => _.signup.user)
+      history.replace('/')
     },
-  });
+  })
+
+  const signupResolver = (data) => {
+    const { email, password } = data
+    signup({
+      variables: {
+        email,
+        password,
+      },
+    })
+  }
 
   return (
-    <Form
-      onSubmit={(e) => {
-        e.preventDefault();
-        signup({
-          variables: {
-            email,
-            password,
-          },
-        });
-      }}
-    >
-      <Form.Group controlId="formBasicEmail">
+    <Form onSubmit={handleSubmit(signupResolver)}>
+      <Form.Group>
         <Form.Label>Email address</Form.Label>
         <Form.Control
-          type="email"
+          type="text"
+          name="email"
           placeholder="Enter email"
-          onChange={(e) => setEmail(e.target.value)}
+          ref={register}
+          isInvalid={!!errors.email}
         />
+        <Form.Control.Feedback type="invalid">
+          {errors.email?.message}
+        </Form.Control.Feedback>
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
-
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group>
         <Form.Label>Password</Form.Label>
         <Form.Control
           type="password"
+          name="password"
           placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
+          ref={register}
+          isInvalid={!!errors.password}
         />
+        <Form.Control.Feedback type="invalid">
+          {errors.password?.message}
+        </Form.Control.Feedback>
       </Form.Group>
-      <Form.Group controlId="formBasicPassword">
+      <Form.Group>
         <Form.Label>Confirm Password</Form.Label>
         <Form.Control
           type="password"
+          name="confirmPassword"
           placeholder="ConfirmPassword"
-          onChange={(e) => setConfirmPassword(e.target.value)}
+          ref={register}
+          isInvalid={!!errors.confirmPassword}
         />
-      </Form.Group>
-      <Form.Group controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Control.Feedback type="invalid">
+          {errors.confirmPassword?.message}
+        </Form.Control.Feedback>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
       </Button>
     </Form>
-  );
-};
+  )
+}
 
-export default Signup;
+export default Signup
